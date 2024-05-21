8ff02f6cd8fa17985b003919c5b38f27e326d4aa
IneichenBBZW <98276716+IneichenBBZW@users.noreply.github.com>
2024-05-21 09:18:52 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+import { ToDo } from './todo.js';
+
+let todos = [];
+
+function updateToDoListOnScreen() {
+  const todoListElement = document.getElementById('todolist');
+
+  // Liste leeren
+  todoListElement.innerHTML = '';
+
+  // ToDo's einfügen
+  for (const todo of todos.sort((a, b) => a.titel.localeCompare(b.titel))) {
+    const toDoListEntry = todo.element();
+    todoListElement.appendChild(toDoListEntry);
+  }
+
+  // offene ToDo's
+  const offeneToDos = todos.filter((offen) => !offen.erledigt);
+  const elementAnzahl = document.getElementById('anzahl');
+  elementAnzahl.textContent = `${offeneToDos.length} ToDo's offen`;
+
+  // Im Local Storage speichern
+  localStorage.setItem('todos', JSON.stringify(todos, ['titel', 'erledigt']));
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Aus Local Storage auslesen
+  const savedTodos = JSON.parse(localStorage.getItem('todos'));
+
+  if (savedTodos) {
+    for (const savedToDo of savedTodos) {
+      const todo = new ToDo(savedToDo.titel, savedToDo.erledigt);
+      todo.addEventListener('loeschen', (e) => {
+        const index = todos.indexOf(e.target);
+        todos.splice(index, 1);
+        updateToDoListOnScreen();
+      });
+      todo.addEventListener('erledigt', (e) => {
+        updateToDoListOnScreen();
+      });
+      todos.push(todo);
+    }
+  }
+
+  updateToDoListOnScreen();
+
+  const neuesToDoElement = document.getElementById('neuesToDo');
+  neuesToDoElement.addEventListener('keydown', (event) => {
+    if (event.code === 'Enter') {
+      const todo = new ToDo(neuesToDoElement.value, false);
+      todos.push(todo);
+
+      neuesToDoElement.value = '';
+
+      todo.addEventListener('loeschen', (e) => {
+        const index = todos.indexOf(e.target);
+        todos.splice(index, 1);
+        updateToDoListOnScreen();
+      });
+
+      todo.addEventListener('erledigt', (e) => {
+        updateToDoListOnScreen();
+      });
+
+      updateToDoListOnScreen();
+    }
+  });
+  document.getElementById('aufraeumen').addEventListener('click', (event) => {
+    todos = todos.filter((obj) => !obj.erledigt);
+    updateToDoListOnScreen();
+  });
+});
